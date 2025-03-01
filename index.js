909173c056c3a48a0a765e9c9b9924517be27654
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-03-01 13:10:10 +0600

    setup done

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const { default: mongoose } = require('mongoose');
+const app = express();
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+const corsOptions = {
+    origin: true,
+}
+
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+// MongoBD Connection
+mongoose.set('strictQuery', false)
+const connectDB = async() => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("MongoDB database is connected");
+    } catch (error) {
+        console.log("MongoDB database is failed to connect");
+    }
+}
+
+// Server Running API
+app.get('/', (req, res) => {
+    res.send("CurePoint server is running")
+})
+app.listen(port, () => {
+    connectDB();
+    console.log(`This server is running on port: ${port}`);
+})
