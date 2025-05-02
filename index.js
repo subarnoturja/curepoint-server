8a7693a76c386c5c311228a2434dedcec44b0e01
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-05-03 00:52:14 +0600

    changes are made

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ const corsOptions = {
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
-app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/users', userRoute);
-app.use('/api/v1/doctors', doctorRoute);
-app.use('/api/v1/reviews', reviewRoute);
-app.use('/api/v1/bookings', bookingRoute);
+app.use('/auth', authRoute);
+app.use('/users', userRoute);
+app.use('/doctors', doctorRoute);
+app.use('/reviews', reviewRoute);
+app.use('/bookings', bookingRoute);
 
 // MongoBD Connection
 mongoose.set('strictQuery', false)
